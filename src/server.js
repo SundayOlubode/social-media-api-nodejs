e4f41ee48f74bc92f9211cc20d0672238b8fb362
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-01-24 21:29:55 +0530

    initial commit

--- src/server.js
+++ src/server.js
@@ -27,6 +27,14 @@ if (process.env.NODE_ENV !== "production") {
 connectDatabase();
 
 
+// Cloudinary Setup
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
