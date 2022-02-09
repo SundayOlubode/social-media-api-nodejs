bfd3d74d4cd9d6cbef1a131e7961a16ca186a98b
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-02-10 01:38:59 +0530

    update changes & bug fixes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,17 +8,27 @@ const User = require("../models/UserModel");
 // Authenticated User
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
 
-    const { token } = req.cookies;
+    const bearerHeader = req.headers['authorization'];
 
-    if (!token) {
-        return next(new ErrorHandler("Please login to account.", 400));
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+
+        token = bearerToken;
+
+        if (!token) {
+            return next(new ErrorHandler("Please login to account.", 400));
+        }
+
+        const userData = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = await User.findById(userData.id);
+
+        next();
     }
-
-    const userData = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = await User.findById(userData.id);
-
-    next();
+    else {
+        return next(new ErrorHandler("Auth token not found.", 400));
+    }
 
 })
 
