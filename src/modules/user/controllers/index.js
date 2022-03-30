21b81d4a1b9d2a78329c6813f2c81cf8fb954933
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-03-31 04:51:48 +0530

    upload image endpoint bug fixed and updated

--- src/modules/user/controllers/index.js
+++ src/modules/user/controllers/index.js
@@ -489,9 +489,9 @@ export const uploadAvatar = catchAsyncError(async (req, res, next) => {
 
                 await user.save();
 
-                fs.unlink(fileTempPath, (err) => {
-                    if (err) console.log(err);
-                })
+                // fs.unlink(fileTempPath, (err) => {
+                //     if (err) console.log(err);
+                // })
 
                 res.status(200).json({
                     success: true,
@@ -500,9 +500,9 @@ export const uploadAvatar = catchAsyncError(async (req, res, next) => {
 
             }).catch((err) => {
 
-                fs.unlink(fileTempPath, (fileErr) => {
-                    if (fileErr) console.log(fileErr);
-                })
+                // fs.unlink(fileTempPath, (fileErr) => {
+                //     if (fileErr) console.log(fileErr);
+                // })
 
                 console.log(err);
 
